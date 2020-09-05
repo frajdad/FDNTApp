b56956d65dc19f9071b35ca609d19684edf9880b
Łukasz Kamiński <kaminski3.141@gmail.com>
2020-09-05 22:31:32 +0200

    Zmiana numerów wersji

--- app/src/main/java/fdnt/app/android/post/PostItemFragment.java
+++ app/src/main/java/fdnt/app/android/post/PostItemFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -159,9 +158,6 @@ public class PostItemFragment extends Fragment {
             try {
                 super.onLayoutChildren(recycler, state);
             } catch (IndexOutOfBoundsException e) {
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
-                alertDialog.setMessage("Wystąpił problem z ładowaniem wiadomości.");
-                alertDialog.show();
             }
         }
     }
