02189914195592fe49776fd1408a1f0d2d5bd2ef
Łukasz Kamiński <kaminski3.141@gmail.com>
2020-11-07 17:17:55 +0100

    Zmieniono częstotliwość działania w tle

--- app/src/main/java/fdnt/app/android/notifications/Util.java
+++ app/src/main/java/fdnt/app/android/notifications/Util.java
@@ -11,8 +11,8 @@ public class Util {
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             ComponentName serviceComponent = new ComponentName(context, NotificationStartService.class);
             JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
-            builder.setMinimumLatency(60 * 1000); // wait at least 1 minute
-            builder.setOverrideDeadline(15 * 60 * 1000); // maximum delay 15 minutes
+            builder.setMinimumLatency(15 * 60 * 1000); // wait at least 1 minute
+            builder.setOverrideDeadline(30 * 60 * 1000); // maximum delay 15 minutes
             builder.setRequiresCharging(false); // we don't care if the device is charging or not
             JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
             jobScheduler.schedule(builder.build());
